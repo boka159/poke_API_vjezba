41b005a59f112e70982c7b8bf53c6ca15f717678
programer10 <programer10@ucione.local>
2023-03-30 18:06:49 +0200

    kreiranje tablice s poke API podacima

---
+++ JS/pokemon-data.js
@@ -0,0 +1,23 @@
+let request = new XMLHttpRequest();
+
+//priprema poziva na (pokemon) API
+request.open("GET", "https://pokeapi.co/api/v2/pokemon-color/yellow", true);
+
+function popuniPokemone(){
+    const resp = JSON.parse(request.response);
+    const sourceHTML = document.getElementById("lista-pokemona").innerHTML;
+    const template = Handlebars.compile(sourceHTML);
+
+    const ctxData = {pokemon: resp.pokemon_species.slice(0,20)};
+    const html = template(ctxData);
+
+    document.getElementById("div-pokemoni").innerHTML = html;
+}
+
+//funkcija koja će se pozvati na loadanju stranice
+request.onload = function(){
+    popuniPokemone();
+}
+
+//pošalji request na (pokemon) API
+request.send();
