694b6fd0f1e58de984f650aa2a7d222eb4bb97ca
programer10 <programer10@ucione.local>
2023-04-03 19:08:12 +0200

    nastavak zadatka s PokeAPI, dodavanje zebra uzorka tablici, skrivanje pokemona na P

--- JS/pokemon-data.js
+++ JS/pokemon-data.js
@@ -23,16 +23,49 @@ function popuniPokemone(){
     const sourceHTML = document.getElementById("lista-pokemona").innerHTML;
     const template = Handlebars.compile(sourceHTML);
 
-    const ctxData = {pokemon: resp.pokemon_species.slice(0,20)};
+    const ctxData = {pokemon: resp.pokemon_species.slice(0,20), tableClass: 'table'};
     const html = template(ctxData);
 
     document.getElementById("div-pokemoni").innerHTML = html;
-    $('[data-bs-toggle=popover]').popover({html:true});
+    $('[data-bs-toggle="popover"]').popover();
+}
+
+function dodajPruge(){
+    $("table tr").removeClass("pruge");
+    $("table tr:even").addClass("pruge");
+}
+
+function dodajHeaderBoju(){
+    $("table th").css("color", "darkblue");
+    $("table th").css("background-color", "green");
+}
+
+function nakon2Sekunde(){
+    setTimeout(function(){
+        let myPokemonP = $("table td a:contains('p')").filter(function(){
+            return this.innerHTML.indexOf('p') == 0;
+        });
+        myPokemonP.closest("tr").remove();
+        dodajPruge();
+
+        console.log("skrivenih: " + myPokemonP.length);
+        $('<div id="skriveni"></div>')
+            .insertAfter($('#div-pokemoni'))
+            .text("Skrivenih: " + myPokemonP.length);
+    }, 2000);
+}
+
+//funkcija koja se izvršava nakon loadanja i određuje redoslijed ostalih funckija (ako imamo više)
+function odradiOstalo(){
+    dodajPruge();
+    dodajHeaderBoju();
+    nakon2Sekunde();
 }
 
 //funkcija koja će se pozvati na loadanju stranice
 request.onload = function(){
     popuniPokemone();
+    odradiOstalo();
 }
 
 //pošalji request na (pokemon) API
